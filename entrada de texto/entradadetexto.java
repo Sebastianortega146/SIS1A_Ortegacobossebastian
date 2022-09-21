59d6dbcf72c6b557e2cf588a098fe86dfe4d62e6
sebastianortega146 <s.ortega.c173@edu.utc.mx>
2022-09-21 08:50:07 -0500

    nuevo archivo

---
+++ entrada de texto/entradadetexto.java
@@ -0,0 +1,14 @@
+class Entradadetexto {
+//metodo principal 
+
+public static void main (String[] arg){
+//estructura basuca 
+//declaracion de variables 
+String nombre;
+System.out.println("Por favor introduce tu nombre:");
+nombre = System.console().readLine();
+
+System.out.println("Hola" + nombre + "Bienvenido a la tronadrea 1_1");
+
+}
+}
