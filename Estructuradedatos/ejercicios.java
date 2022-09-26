f4525d01091af627071396ecd9b873d3cfe26394
sebastianortega146 <s.ortega.c173@edu.utc.mx>
2022-09-26 14:21:56 -0500

    nuevo archivo

---
+++ Estructuradedatos/ejercicios.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+class ejercicios {
+    /** vamos a hacer una calculadora, un conversor de unidades creacion de cuadros y de movimiento */
+
+    // los metodos son llas operaciones de un objeto o de una clase, tambien llamado comportamiento
+
+    //objetos
+
+    Scanner entrada = new Scanner(System.in);
+    
+    //vamos a ser un metodo para el menu del programa 
+    public void menu(){
+
+        public void Ejercicio1(){
+
+        }
+
+        public void Ejercicio2(){
+
+        }
+
+        public void Ejercicio3(){
+
+        }
+
+        public void Ejercicio4(){
+            
+        }
+    }
+    //menu del programa 
+    System.out.println("ejercicios de yo para tu");
+    System.out.println("a,-calculadora");
+    System.out.println("b,-conversion de unidades");
+    System.out.println("c,-crear cuadros magicos");
+    System.out.println("d,- deplazamiento de un cuadro");
+    System.out.println("eliga una opcion :D");
+
+    op = entrada.next().charAt(index:0);
+   
+    switch (op){
+        case 'a'
+        //implementos metodos
+        Ejercicio1();
+        break;
+        case 'b'
+        //implementos metodos 
+        Ejercicio2();
+        break;
+        default:
+        System.out.println("gracias por jugar");
+
+    }
+
+
+}
