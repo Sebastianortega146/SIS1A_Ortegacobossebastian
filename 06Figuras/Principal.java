0f66f43e1577a80febfdf00356159dd7f01f0254
Sebastianortega146 <113538825+Sebastianortega146@users.noreply.github.com>
2022-10-03 18:15:38 -0500

    figuras
    figuras

---
+++ 06Figuras/Principal.java
@@ -0,0 +1,8 @@
+public class Principal{
+
+    public static void main(String[] args){
+        //instancia de la clase
+        figuritas objfigura = new figuritas();
+        objfigura.menu();
+    }
+}
