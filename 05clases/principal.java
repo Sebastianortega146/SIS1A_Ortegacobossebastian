6b54d9744c8a91b81a7e04ac6bf40dc8f9657c06
Sebastianortega146 <113538825+Sebastianortega146@users.noreply.github.com>
2022-10-03 11:49:12 -0500

    Add files via upload

---
+++ 05clases/principal.java
@@ -0,0 +1,10 @@
+class principal {
+
+    //metodo principal
+    public static void main(String[] args){
+        //vamos a crear una instancia
+        ejercicios ejer = new ejercicios();
+        ejer.menu();
+    }
+    
+}
